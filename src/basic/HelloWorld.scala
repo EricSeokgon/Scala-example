c86c4a9d909729e1bb57c2aeb128dec464e982c3
hadesyi <hadesleeq@gmail.com>
2016-01-22 11:08:43 +0900

    scala basic

--- src/basic/HelloWorld.scala
+++ src/basic/HelloWorld.scala
@@ -13,4 +13,26 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     print("Hello World")
   }
+
+  println(1 + 1)
+  val two = 1 + 1
+  println(two)
+
+  var name = "steve"
+  println(name)
+
+  name = "marius"
+  println(name)
+
+  def addOne(m: Int): Int = m + 1
+
+  val three = addOne(2)
+  println(three)
+
+  def five() = 2 + 3
+  println(five())
+  println(five)
+
+
+
 }
