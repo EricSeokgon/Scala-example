a7c9da88ffc4ce6650b4dcd6034bb66c0db39dc0
hadesyi <hadesleeq@gmail.com>
2016-01-23 01:07:17 +0900

    클래스 Buffer의 멤버로써 완전히 확정되지 않은 값과 추상 타입을 정의

---
+++ src/abstract/Buffer.scala
@@ -0,0 +1,18 @@
+package abstract
+
+package
+
+abstract /**
+  * Project: Scala-example
+  * FileName: Buffer
+  * Date: 2016-01-23
+  * Time: 오전 1:06
+  * Author: Hadeslee 
+  * Note: 
+  * To change this template use File | Settings | File Templates.
+  */
+trait Buffer {
+  type T
+  val elementL: T
+
+}
