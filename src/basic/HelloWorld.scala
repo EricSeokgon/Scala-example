50afb2f6b777fe21e4d618ca578c2ca0583e72c3
hadesyi <hadesleeq@gmail.com>
2016-01-22 10:56:32 +0900

    Initial commit

---
+++ src/basic/HelloWorld.scala
@@ -0,0 +1,16 @@
+package basic
+
+/**
+  * Project: Scala-example
+  * FileName: HelloWorld
+  * Date: 2016-01-22
+  * Time: 오전 10:49
+  * Author: Hadeslee 
+  * Note: 
+  * To change this template use File | Settings | File Templates.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    print("Hello World")
+  }
+}
