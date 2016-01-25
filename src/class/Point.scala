a1fc52f3a55b5962d77285300e80049e44998f2c
hadesyi <hadesleeq@gmail.com>
2016-01-25 14:11:42 +0900

    ‘Point’ 클래스의 정의

---
+++ src/class/Point.scala
@@ -0,0 +1,27 @@
+package class
+
+package
+
+class
+
+/**
+  * Project: Scala-example
+  * FileName: Point
+  * Date: 2016-01-25
+  * Time: 오후 2:09
+  * Author: Hadeslee 
+  * Note: 
+  * To change this template use File | Settings | File Templates.
+  */
+class Point(xc: Int, yc: Int) {
+  var x: Int = xc
+  var y: Int = yc
+
+  def move(dx: Int, dy: Int) {
+    x = x + dx
+    y = y + dy
+  }
+
+  override def toString(): String = "(" + x + ", " + y + ")";
+
+}
