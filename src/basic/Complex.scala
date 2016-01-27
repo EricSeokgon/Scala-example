557f75b4e3161ff6967973911ad6ea83297b5a92
hadesyi <hadesleeq@gmail.com>
2016-01-27 17:24:33 +0900

    class 정의

---
+++ src/basic/Complex.scala
@@ -0,0 +1,17 @@
+package basic
+
+/**
+  * Project: Scala-example
+  * FileName: Complex
+  * Date: 2016-01-27
+  * Time: 오후 5:23
+  * Author: Hadeslee 
+  * Note: 
+  * To change this template use File | Settings | File Templates.
+  */
+class Complex(real: Double, imaginary: Double) {
+  def re() = real
+
+  def im() = imaginary
+
+}
