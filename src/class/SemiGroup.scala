54a4404ba6d44014341c5080fc3a84d442238fa4
hadesyi <hadesleeq@gmail.com>
2016-02-02 01:03:40 +0900

    암시적 파라미터

---
+++ src/class/SemiGroup.scala
@@ -0,0 +1,32 @@
+package class
+
+/**
+  * Project: Scala-example
+  * FileName: SemiGroup
+  * Date: 2016-02-02
+  * Time: 오전 1:02
+  * Author: Hadeslee 
+  * Note: 
+  * To change this template use File | Settings | File Templates.
+  */
+abstract class SemiGroup[A] {
+  def add(x: A, y: A): A
+}
+abstract class Monoid[A] extends SemiGroup[A] {
+  def unit: A
+}
+object ImplicitTest extends App {
+  implicit object StringMonoid extends Monoid[String] {
+    def add(x: String, y: String): String = x concat y
+    def unit: String = ""
+  }
+  implicit object IntMonoid extends Monoid[Int] {
+    def add(x: Int, y: Int): Int = x + y
+    def unit: Int = 0
+  }
+  def sum[A](xs: List[A])(implicit m: Monoid[A]): A =
+    if (xs.isEmpty) m.unit
+    else m.add(xs.head, sum(xs.tail))
+  println(sum(List(1, 2, 3)))
+  println(sum(List("a", "b", "c")))
+}
