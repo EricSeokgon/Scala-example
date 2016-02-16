ba3dffb2bc798b34dd195c25d10e0bfff8a2049d
hadesyi <hadesleeq@gmail.com>
2016-02-17 00:51:37 +0900

    상위 타입 경계

---
+++ src/basic/Similar.scala
@@ -0,0 +1,14 @@
+package basic
+
+/**
+  * Project: Scala-example
+  * FileName: Similar
+  * Date: 2016-02-17
+  * Time: 오전 12:51
+  * Author: Hadeslee 
+  * Note: 
+  * To change this template use File | Settings | File Templates.
+  */
+trait Similar {
+  def isSimilar(x: Any): Boolean
+}
