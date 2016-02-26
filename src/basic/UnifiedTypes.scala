7c5c7e9debd5c825e7108847e416d6e948d2afe0
hadesyi <hadesleeq@gmail.com>
2016-02-27 00:14:36 +0900

    스칼라 클래스 계층 구조

---
+++ src/basic/UnifiedTypes.scala
@@ -0,0 +1,23 @@
+package basic
+
+/**
+  * Project: Scala-example
+  * FileName: UnifiedTypes
+  * Date: 2016-02-27
+  * Time: 오전 12:14
+  * Author: Hadeslee 
+  * Note: 
+  * To change this template use File | Settings | File Templates.
+  */
+object UnifiedTypes extends App {
+  val set = new scala.collection.mutable.LinkedHashSet[Any]
+  set += "This is a string"  // 문자열을 추가한다
+  set += 732                 // 숫자를 추가한다
+  set += 'c'                 // 캐릭터를 추가한다
+  set += true                // 불리언 값을 추가한다
+  set += main _              // 메인 함수를 추가한다
+  val iter: Iterator[Any] = set.iterator
+  while (iter.hasNext) {
+    println(iter.next.toString())
+  }
+}
